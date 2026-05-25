d7f93d5867cf3dcb01e2c5ddd4de5e66a4996f12
ZengPu77 <zengpu@nudt.edu.cn>
2026-05-25 17:06:16 +0800

    🔧 fix(panel): 修复菜单点击外部区域不自动关闭的问题
    - 移除自定义 captured-event 外部点击监听器
    - 改用 GNOME Shell 内置的 PopupMenuManager 管理菜单开关
    - 注册/注销 Main.panel.menuManager 确保标准交互行为
    
    🔖 v1.0.4

--- modules/panelIndicator.js
+++ modules/panelIndicator.js
@@ -4,6 +4,7 @@ import Gio from 'gi://Gio';
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
@@ -68,6 +69,7 @@ class CodeUsageIndicator extends PanelMenu.Button {
         this.add_child(this._box);
 
         this._createMenu();
+        Main.panel.menuManager.addMenu(this.menu);
         this._updateDisplayMode();
         this._updateIconVisibility();
 
@@ -491,6 +493,7 @@ class CodeUsageIndicator extends PanelMenu.Button {
 
     destroy() {
         this._stopTimer();
+        Main.panel.menuManager.removeMenu(this.menu);
         if (this._settingsChangedId) {
             this._settings.disconnect(this._settingsChangedId);
             this._settingsChangedId = null;
